51eedc266837633b9cada08b42e54727333201c4
Rick <derruette@yahoo.fr>
2024-11-24 23:39:47 +0100

    ingtegration of openAI

--- springboot-backend/src/main/java/net/javaguides/springboot/service/AIService.java
+++ springboot-backend/src/main/java/net/javaguides/springboot/service/AIService.java
@@ -1,24 +1,26 @@
 package net.javaguides.springboot.service;
 
-import org.springframework.ai.bedrock.jurassic2.BedrockAi21Jurassic2ChatModel;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 
 @Service
 public class AIService {
 
-    private final BedrockAi21Jurassic2ChatModel chatModel;
+    private final ChatClient chatClient;
 
-    public AIService(BedrockAi21Jurassic2ChatModel chatModel) {
-        this.chatModel = chatModel;
+    public AIService(ChatClient.Builder chatClientBuilder) {
+        this.chatClient = chatClientBuilder.build();
     }
-
-    public String getOneQuestion(String message) {
-        try {
-            return chatModel.call(message);
-        } catch (Exception e) {
-            return e.getMessage();
-        }
+    public String getOneQuestion(String prompt) {
+        return chatClient.prompt()
+                .user(prompt)
+                .call()
+                .content();
     }
 
 }
